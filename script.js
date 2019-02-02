152d8451f0a9f80924df9aecb14174f74252fd6a
Thanhthanh <thanhthanh@csedge.org>
2019-02-02 15:06:07 -0800

    Update, this GitHub feature doesn't really help!

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-window.onload = function() {
-  //write all of your code inside here
+//When I click the play button
+// it will move to a random location on the page
 
+let button = document.getElementById("play-button");
 
+button.addEventListener("click", moveToRandom);
+
+function moveToRandom() {
+  // get a random number, reassign value for "top"
+  let newTopValue = Math.floor(Math.random() * 100);
+  // get a random number, reassign value for "left"
+  let newLeftValue = Math.floor(Math.random() * 100);
+
+  console.log(newTopValue);
+  console.log(newLeftValue);
+
+  button.style.top = newTopValue + "%";
+  button.style.left = newLefValue + "%";
 }
